76184ea40b1ba9062a365aff9e1c85edfd1c10e7
vinitparamane <vinitparamane2810@gmail.com>
2025-09-10 17:06:55 +0530

    encapsulation

---
+++ src/Daily_Practice/Encapsulation/EncapsulationDemo.java
@@ -0,0 +1,18 @@
+package Daily_Practice.Encapsulation;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	OopsConceptDemo obj = new OopsConceptDemo();
+		
+		obj.setSerialNum(101);
+		obj.setName("Pooja");
+		obj.setAge(21);
+		
+		System.out.println(obj);
+
+	}
+
+}
