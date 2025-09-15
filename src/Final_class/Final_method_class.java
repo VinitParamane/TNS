573521166273083ec0171809fcd8a31c0ce2edef
vinitparamane <vinitparamane2810@gmail.com>
2025-09-15 15:36:45 +0530

    Final_class

---
+++ src/Final_class/Final_method_class.java
@@ -0,0 +1,16 @@
+package Final_class;
+
+public class Final_method_class {
+final int a = 50;
+	
+	final void show()
+	{
+		System.out.println("Value of a: "+a);
+	}
+
+//	final void show()
+//	{
+//		System.out.println("show Method");
+//	}
+
+}
---
+++ src/Final_class/Final_method_test.java
@@ -0,0 +1,16 @@
+package Final_class;
+
+public class Final_method_test extends Final_method_class{
+
+//	void show()
+//	{
+//		System.out.println("Value of a: "+a);
+//	}
+
+
+	public static void main(String[] args) {
+
+		Final_method_class f1 = new Final_method_class();
+		f1.show();
+	}
+}
